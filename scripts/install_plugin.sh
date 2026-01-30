ab74a9b8550cbb46be6a1cb959bb16420f3d5d7b
Florian Limburg <florian.limburg@actian.com>
2026-01-30 09:19:56 +0100

    throwing in a verify during install

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -76,12 +76,13 @@ mkdir -p "${HELM_PLUGIN_DIR}/releases/v${version}"
 
 # Download with curl if possible.
 if [ -x "$(which curl 2>/dev/null)" ]; then
-    curl -sSL "${url}" -o "${HELM_PLUGIN_DIR}/releases/v${version}.tar.gz"
-    curl -sSL "${url}.prov" -o "${HELM_PLUGIN_DIR}/releases/v${version}.tgt.prov"
+    curl -sSL "${url}" -o "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz"
+    curl -sSL "${url}.prov" -o "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz.prov"
 else
-    wget -q "${url}" -O "${HELM_PLUGIN_DIR}/releases/v${version}.tgz"
-    wget -q "${url}.prov" -O "${HELM_PLUGIN_DIR}/releases/v${version}.tgz.prov"
+    wget -q "${url}" -O "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz"
+    wget -q "${url}.prov" -O "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz.prov"
 fi
-tar xzf "${HELM_PLUGIN_DIR}/releases/v${version}.tgz" -C "${HELM_PLUGIN_DIR}/releases/v${version}"
+helm plugin verify "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz"
+tar xzf "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz" -C "${HELM_PLUGIN_DIR}/releases/v${version}"
 mv "${HELM_PLUGIN_DIR}/releases/v${version}/bin/helm-cm-push" "${HELM_PLUGIN_DIR}/bin/helm-cm-push" || \
     mv "${HELM_PLUGIN_DIR}/releases/v${version}/bin/helm-cm-push.exe" "${HELM_PLUGIN_DIR}/bin/helm-cm-push"

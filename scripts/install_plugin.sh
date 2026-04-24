37c8c5fdbc814cc5d9abdb5c526467824a47b590
JSpencerB <joel.spencer-buff@hcl-software.com>
2026-04-24 14:16:34 +0200

    Update install_plugin.sh
    L86-L88 remove v

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -83,6 +83,6 @@ else
     wget -q "${url}.prov" -O "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz.prov"
 fi
 #helm plugin verify "${HELM_PLUGIN_DIR}/releases/cm-push-v${version}.tgz"
-tar xzf "${HELM_PLUGIN_DIR}/releases/cm-push-v${version}.tgz" -C "${HELM_PLUGIN_DIR}/releases/${version}"
-mv "${HELM_PLUGIN_DIR}/releases/v${version}/cm-push/bin/helm-cm-push" "${HELM_PLUGIN_DIR}/bin/helm-cm-push" || \
-    mv "${HELM_PLUGIN_DIR}/releases/v${version}/cm-push/bin/helm-cm-push.exe" "${HELM_PLUGIN_DIR}/bin/helm-cm-push"
+tar xzf "${HELM_PLUGIN_DIR}/releases/cm-push-${version}.tgz" -C "${HELM_PLUGIN_DIR}/releases/${version}"
+mv "${HELM_PLUGIN_DIR}/releases/${version}/cm-push/bin/helm-cm-push" "${HELM_PLUGIN_DIR}/bin/helm-cm-push" || \
+    mv "${HELM_PLUGIN_DIR}/releases/${version}/cm-push/bin/helm-cm-push.exe" "${HELM_PLUGIN_DIR}/bin/helm-cm-push"
